6b08f81c3fa56d6a413eb95053a76888b754bbf7
Doug Colaizzo <dcolaizzo@gmail.com>
2015-11-15 22:04:35 -0500

    Initial walkie talkie implementation.
    Chrome support verified.

---
+++ index.js
@@ -0,0 +1,18 @@
+var express = require('express'),
+    app = express(),
+    http = require('http').Server(app),
+    io = require('socket.io')(http);
+
+app.use(express.static('public'));
+
+io.on('connection', function(socket) {
+    socket.join('walkie-talkie');
+
+    socket.on('talk', function(msg) {
+        socket.broadcast.to('walkie-talkie').emit('talk', msg);
+    })
+});
+
+http.listen(3011, function() {
+    console.log('listening on *:3011');
+});
